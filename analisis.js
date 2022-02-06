1d78870a357217bd0f17771116352d9867dde6e2
Jose Luis Luna <jluna0609@gmail.com>
2022-02-06 09:45:16 -0300

    final del curso

---
+++ analisis.js
@@ -0,0 +1,50 @@
+
+function calcularMediaAritmetica(lista) {
+    // let sumaLista = 0;
+    // for (let i= 0; i < lista.length; i++) {
+    //     sumaLista+= lista[i];
+    // }
+    if (lista.length > 0)    {
+    const sumaLista= lista.reduce(
+        function(sumita=0, elemento ) {
+           return sumita+= elemento;
+        }
+    )
+    const promedioLista = sumaLista / lista.length;
+    return promedioLista;
+    }
+    else{
+        console.log("la lista esta vacia");
+    }
+}
+
+function calcularMediana(lista) {
+    // const lista1=[ 100, 200, 300, 3500, 50000, 65000];
+        let lista1=[];
+        lista1 = lista;
+        lista1.sort(function(a, b){return a-b});
+        let mitadLista1= 0;
+        if (lista1.length > 0) {
+            mitadLista1= lista1.length / 2;
+            mitadLista1=parseInt(mitadLista1);
+            if (lista1.length % 2 === 0){
+                return calcularMediaAritmetica([lista1[mitadLista1],
+                                            lista1[mitadLista1-1]]);
+            }
+            else {  
+            return lista1[mitadLista1];
+            }   
+        }
+        else{
+        console.log("la lista esta vacia");
+        }
+    }
+
+
+const salariosCol= colombia.map(
+    function(tipo) {
+        return tipo.salary;
+    })
+
+
+console.log(calcularMediana(salariosCol));
---
+++ salarios.js
@@ -0,0 +1,14 @@
+const colombia= [];
+colombia.push({
+    name:"juanita",
+    salary:1000});
+colombia.push({
+    name:"sara",
+    salary:1400});
+colombia.push({
+    name:"alberto",
+    salary:1500});
+colombia.push({
+    name:"esteban",
+    salary:3500});    
+console.log(colombia);
